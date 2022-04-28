af962e47476da409050e3bd4eaa07a2247e2dd8b
Mohammad saeedi <103640961+Gushatku@users.noreply.github.com>
2022-04-28 10:35:59 +0430

    Add files via upload

---
+++ %magic code.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+700*900
+
+
+# In[5]:
+
+
+3315*3315
+
+
+# In[6]:
+
+
+5+5
+
+
+# In[7]:
+
+
+12*2
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('autocall', '')
+
+
+# In[12]:
+
+
+get_ipython().run_line_magic('autocall', '{mode}')
+
+
+# In[13]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[14]:
+
+
+get_ipython().run_line_magic('cd', '')
+
+
+# In[16]:
+
+
+get_ipython().run_line_magic('dhist', '')
+
+
+# In[17]:
+
+
+get_ipython().run_line_magic('edit', '')
+
+
+# In[18]:
+
+
+get_ipython().run_line_magic('env', '')
+
+
+# In[19]:
+
+
+get_ipython().run_line_magic('env', 'var')
+
+
+# In[21]:
+
+
+get_ipython().run_line_magic('env', 'var wall')
+
+
+# In[26]:
+
+
+get_ipython().run_line_magic('gui', 'wx')
+
+
+# In[27]:
+
+
+get_ipython().run_line_magic('gui', 'qt4 | qt')
+
+
+# In[28]:
+
+
+get_ipython().run_line_magic('lsmagic', '')
+
+
+# In[31]:
+
+
+get_ipython().run_line_magic('matplotlib', '')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('notebook', '')
+
+
+# In[32]:
+
+
+get_ipython().run_line_magic('pinfo', '')
+
+
+# In[33]:
+
+
+get_ipython().run_line_magic('precision', '')
+
+
+# In[34]:
+
+
+get_ipython().run_line_magic('pylab', '')
+
+
+# In[38]:
+
+
+get_ipython().run_line_magic('recall', '')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('r', '')
+
+
+# In[39]:
+
+
+get_ipython().run_line_magic('run', '')
+
+
+# In[40]:
+
+
+get_ipython().run_line_magic('timeit', '')
+
+
+# In[41]:
+
+
+get_ipython().run_line_magic('who', '')
+
+
+# In[ ]:
+
+
+
+
